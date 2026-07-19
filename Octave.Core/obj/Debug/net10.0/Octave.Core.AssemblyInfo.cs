24cfd07faf3a3efd9a314aaa64629d5c56d89d0c
Aditya Pratap Singh <adityapratapsingh_23me025@dtu.ac.in>
2026-07-19 08:31:39 +0530

    something something bitch

--- Octave.Core/obj/Debug/net10.0/Octave.Core.AssemblyInfo.cs
+++ Octave.Core/obj/Debug/net10.0/Octave.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Octave.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bf08045e980d8d08da90608ffb856841fcfad4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89653eefecbeacacf50a44ef7c1cb0afa47772b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Octave.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Octave.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
